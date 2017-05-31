e55fe2b2827e08c7bb2523f4adf7182b1505d762
Ben Hernandez <ben.hernandez@wipro.com>
2017-05-30 21:53:10 -0600

    no cd and lap times

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
-import { Observable } from 'rxjs/Rx';
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs/Rx';
 
 interface Interval {
   start: number;
@@ -7,7 +7,6 @@ interface Interval {
 }
 
 @Component({
-  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-timer',
   templateUrl: './timer.component.html',
   styleUrls: ['./timer.component.css']
@@ -17,11 +16,22 @@ export class TimerComponent implements OnInit {
   intervals: Interval[] = [];
   totalTime = 0;
   paused = null;
+  updateSubscription: Subscription;
 
-  constructor(private cd: ChangeDetectorRef) { }
+  constructor() { }
 
   ngOnInit() {
-    Observable.interval(10).subscribe(this.updateTime.bind(this));
+
+  }
+
+  startObservable() {
+    this.updateSubscription = Observable.interval(10).subscribe(this.updateTime.bind(this));
+  }
+
+  stopObservable() {
+    if (this.updateSubscription) {
+      this.updateSubscription.unsubscribe();
+    }
   }
 
   start() {
@@ -30,23 +40,30 @@ export class TimerComponent implements OnInit {
     this.intervals.push({
       start: Date.now(),
     });
+    this.startObservable();
   }
 
   reset() {
     this.paused = null;
     this.totalTime = 0;
     this.intervals = [];
-    this.cd.markForCheck();
   }
 
   pauseAndResume() {
     if (this.intervals[this.intervals.length - 1].stop) {
       this.startNewInterval();
       this.paused = false;
+      this.startObservable();
     } else {
       this.intervals[this.intervals.length - 1].stop = Date.now();
       this.paused = true;
+      this.stopObservable();
     }
+  }
+
+  getIntervalTime(interval: Interval) {
+    const stopTime = interval.stop ? interval.stop : Date.now();
+    return stopTime - interval.start;
   }
 
   private startNewInterval() {
@@ -58,9 +75,8 @@ export class TimerComponent implements OnInit {
   private updateTime() {
     this.totalTime = this.intervals.reduce((total, interval: Interval) => {
       const stopTime = interval.stop ? interval.stop : Date.now();
-      total += stopTime - interval.start;
+      total += this.getIntervalTime(interval);
       return total;
     }, 0);
-    this.cd.markForCheck();
   }
 }
